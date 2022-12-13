c8d0cf958965502ec818bfd3c039a9882f2c53c5
kavi307 <ajkavitha7@gmail.com>
2022-12-13 17:23:16 +0530

    area of traingle

---
+++ index.js
@@ -0,0 +1,6 @@
+var a,b,h,traingle;
+        a=0.5;
+        b=prompt(parseInt("enter the value"));
+        h=prompt(parseInt("enter the radius"));
+        traingle=a*b*h;
+        console.log(traingle);
